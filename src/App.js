f4a780dad90821c86501e453cfd89b22fd048e04
LvnReyS16 <elvinreysa@gmail.com>
2020-12-12 05:57:02 -0800

    commited

--- src/App.js
+++ src/App.js
@@ -77,16 +77,8 @@ const routes = [
     children: [
       {
         path: "/",
-        element: <p>Overview</p>
-      },
-      {
-        path: "/new-users",
-        element: <p>New users</p>
-      },
-      {
-        path: "/sales",
-        element: <p>Sales</p>
-      },
+        element: <Patients />
+      }
     ], 
   },
   { 
@@ -104,79 +96,11 @@ const routes = [
       {
         path: "/employees", 
         element: <p>Employees</p> 
+
       },
       {
         path: "/patients", 
-        element: <div class="mt-10 sm:mt-0">
-        <div class="md:grid md:grid-cols-3 md:gap-6">
-          <div class="md:col-span-1">
-            <div class="px-4 sm:px-0">
-              <h3 class="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
-              <p class="mt-1 text-sm text-gray-600">
-                Use a permanent address where you can receive mail.
-              </p>
-            </div>
-          </div>
-          <div class="mt-5 md:mt-0 md:col-span-2">
-            <form action="#" method="POST">
-              <div class="shadow overflow-hidden sm:rounded-md">
-                <div class="px-4 py-5 bg-white sm:p-6">
-                  <div class="grid grid-cols-6 gap-6">
-                    <div class="col-span-6 sm:col-span-3">
-                      <label for="first_name" class="block text-sm font-medium text-gray-700">First name</label>
-                      <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
-                    </div>
-      
-                    <div class="col-span-6 sm:col-span-3">
-                      <label for="last_name" class="block text-sm font-medium text-gray-700">Last name</label>
-                      <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
-                    </div>
-      
-                    <div class="col-span-6 sm:col-span-4">
-                      <label for="email_address" class="block text-sm font-medium text-gray-700">Email address</label>
-                      <input type="text" name="email_address" id="email_address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
-                    </div>
-      
-                    <div class="col-span-6 sm:col-span-3">
-                      <label for="country" class="block text-sm font-medium text-gray-700">Country / Region</label>
-                      <select id="country" name="country" autocomplete="country" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                        <option>United States</option>
-                        <option>Canada</option>
-                        <option>Mexico</option>
-                      </select>
-                    </div>
-      
-                    <div class="col-span-6">
-                      <label for="street_address" class="block text-sm font-medium text-gray-700">Street address</label>
-                      <input type="text" name="street_address" id="street_address" autocomplete="street-address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
-                    </div>
-      
-                    <div class="col-span-6 sm:col-span-6 lg:col-span-2">
-                      <label for="city" class="block text-sm font-medium text-gray-700">City</label>
-                      <input type="text" name="city" id="city" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
-                    </div>
-      
-                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                      <label for="state" class="block text-sm font-medium text-gray-700">State / Province</label>
-                      <input type="text" name="state" id="state" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
-                    </div>
-      
-                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                      <label for="postal_code" class="block text-sm font-medium text-gray-700">ZIP / Postal</label>
-                      <input type="text" name="postal_code" id="postal_code" autocomplete="postal-code" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
-                    </div>
-                  </div>
-                </div>
-                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                  <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Save
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+        element: <Patients />
       }
     ] 
   },
@@ -207,7 +131,7 @@ const routes = [
         element: <p>Overview</p> },
       { 
         path: "/accounts", 
-        element: <p>Account</p> 
+        element: <Accountss /> 
       },
       { 
         path: "/departments", 
@@ -383,6 +307,215 @@ function Login() {
   );
 }
 
+function Patients() {
+  return (
+    <div class="mt-10 sm:mt-0">
+    <div class="md:grid md:grid-cols-3 md:gap-6">
+      <div class="md:col-span-1">
+        <div class="px-4 sm:px-0">
+          <h3 class="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
+          <p class="mt-1 text-sm text-gray-600">
+            Use a permanent address where you can receive mail.
+          </p>
+        </div>
+      </div>
+      <div class="mt-5 md:mt-0 md:col-span-2">
+        <form action="#" method="POST">
+          <div class="shadow overflow-hidden sm:rounded-md">
+            <div class="px-4 py-5 bg-white sm:p-6">
+              <div class="grid grid-cols-6 gap-6">
+
+              <div class="col-span-6 sm:col-span-4">
+                  <label for="patient_type" class="block text-sm font-medium text-gray-700">Patient Type</label>
+                  <select id="Postition" name="Postition" autocomplete="Postition" class="mt-1 block w-80 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm w-80">
+                    <option>Student</option>
+                    <option>Employee</option>
+                   
+                  </select>
+                  </div>
+
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="Postition" class="block text-sm font-medium text-gray-700">Postition</label>
+                  <select id="Postition" name="Postition" autocomplete="Postition" class="mt-1 block w-80 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm w-80">
+                    <option>Registrar</option>
+                    <option>Teacher</option>
+                   
+                  </select>
+                </div>
+
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="Course" class="block text-sm font-medium text-gray-700">Course</label>
+                  <select id="Course" name="Course" autocomplete="Course" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ">
+                    <option>Student</option>
+                    <option>Employee</option>
+                   
+                  </select>
+                </div>
+
+              <div class="col-span-6 sm:col-span-4">
+                  <label for="Student_id" class="block text-sm font-medium text-gray-700">ID</label>
+                  <input type="text" name="Student_id" id="Student_id" autocomplete="Student_id" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-50 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="first_name" class="block text-sm font-medium text-gray-700">First name</label>
+                  <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-80 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+  
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="last_name" class="block text-sm font-medium text-gray-700">Last name</label>
+                  <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-80 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="last_name" class="block text-sm font-medium text-gray-700">Middle Name</label>
+                  <input type="text" name="Middle_name" id="Middle_name" autocomplete="Middle-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-80 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+  
+               
+
+                <div class="col-span-6 sm:col-span-4">
+                  <label for="blood_pressure" class="block text-sm font-medium text-gray-700">Blood Pressure</label>
+                  <input type="text" name="blood_presssure" id="blood_pressure" autocomplete="blood_pressure" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-25 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+
+                <div class="col-span-6 sm:col-span-4">
+                  <label for="Height" class="block text-sm font-medium text-gray-700">Height</label>
+                  <input type="text" name="Height" id="Height" autocomplete="Height" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-25 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+
+                <div class="col-span-6 sm:col-span-4">
+                  <label for="Weight" class="text-sm font-medium text-gray-700">Weight</label>
+                  <div class="flex">
+                  <input type="text" name="Weight" id="Weight" autocomplete="Weight" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-15 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200 "  />
+                  <select id="Course" name="Course" autocomplete="Course" class="mt-1 block wr-10 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-6">
+                    <option>Student</option>
+                    <option>Employee</option>
+                   
+                  </select>
+                  </div>
+                </div>
+
+                <div class="col-span-6 sm:col-span-4">
+                  <label for="Blood_sugar" class="block text-sm font-medium text-gray-700">Blood Sugar</label>
+                  <input type="text" name="Blood_sugar" id="Blood_sugar" autocomplete="Blood_sugar" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-25 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+
+
+              </div>
+            </div>
+            
+            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+            <button type="clear" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Clear
+              </button>
+              <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 m-2">
+                Save
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+            
+  );
+}
+
+function Accountss() {
+  return (
+    <div class="mt-10 sm:mt-0">
+    <div class="md:grid md:grid-cols-3 md:gap-6">
+      <div class="md:col-span-1">
+        <div class="px-4 sm:px-0">
+          <h3 class="text-lg font-medium leading-6 text-gray-900">Account Information</h3>
+          <p class="mt-1 text-sm text-gray-600">
+            Use a permanent address where you can receive mail.
+          </p>
+        </div>
+      </div>
+      <div class="mt-5 md:mt-0 md:col-span-2">
+        <form action="#" method="POST">
+          <div class="shadow overflow-hidden sm:rounded-md">
+            <div class="px-4 py-5 bg-white sm:p-6">
+              <div class="grid grid-cols-6 gap-6">
+
+              <div class="col-span-6 sm:col-span-4">
+                  <label for="account_type" class="block text-sm font-medium text-gray-700">Account Type</label>
+                  <select id="account_type" name="account_type" autocomplete="account_type" class="mt-1 block w-80 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm w-80">
+                    <option>Admin</option>
+                    <option>Doctor</option>
+                    <option>Nurse</option>
+                    <option>Lab Assistance</option>
+                   
+                  </select>
+                  </div>
+
+              
+
+               
+
+             
+
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="first_name" class="block text-sm font-medium text-gray-700">First name</label>
+                  <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-80 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+  
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="last_name" class="block text-sm font-medium text-gray-700">Last name</label>
+                  <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-80 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="last_name" class="block text-sm font-medium text-gray-700">Middle Name</label>
+                  <input type="text" name="Middle_name" id="Middle_name" autocomplete="Middle-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-80 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+  
+               
+
+                <div class="col-span-6 sm:col-span-4">
+                  <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
+                  <input type="username" name="username" id="blood_pressure" autocomplete="username" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-80 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+
+                <div class="col-span-6 sm:col-span-4">
+                  <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                  <input type="password" name="password" id="password" autocomplete="password" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-80 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+
+                <div class="col-span-6 sm:col-span-4">
+                  <label for="Weight" class="block text-sm font-medium text-gray-700">Weight</label>
+                  <input type="text" name="Weight" id="Weight" autocomplete="Weight" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-25 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+
+                <div class="col-span-6 sm:col-span-4">
+                  <label for="Blood_sugar" class="block text-sm font-medium text-gray-700">Blood Sugar</label>
+                  <input type="text" name="Blood_sugar" id="Blood_sugar" autocomplete="Blood_sugar" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-25 shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" />
+                </div>
+
+
+              </div>
+            </div>
+            
+            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+            <button type="clear" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Clear
+              </button>
+              <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 m-2">
+                Save
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+            
+  );
+}
+
+
 
 function Dashboard() {
   return (
@@ -453,22 +586,6 @@ function Transactions() {
             >
               Overview
             
-            </NavLink>
-            <NavLink
-              to="new-users"
-              activeClassName="text-gray-700 bg-gray-100"
-              inactiveClassName="text-gray-500 hover:text-gray-700"
-              className="ml-4 px-2 py-1 font-medium text-xs leading-5 rounded-md"
-            >
-              New users
-            </NavLink>
-            <NavLink
-              to="sales"
-              activeClassName="text-gray-700 bg-gray-100"
-              inactiveClassName="text-gray-500 hover:text-gray-700"
-              className="ml-4 px-2 py-1 font-medium text-xs leading-5 rounded-md"
-            >
-              Sales
             </NavLink>
           </nav>
         </div>
