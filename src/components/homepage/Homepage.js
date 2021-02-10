ec92c7e89be6780cda9b1cabe40f5df6dd8ca381
Haidir Hassan <Studenthaidir@gmail.com>
2021-02-10 08:23:48 +0800

    Home Page

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -5,19 +5,11 @@ import Home from "./Home.js";
 import Footer from "./Footer.js";
 function Homepage() {
     return (
-        <div className="flex flex-col min-h-screen overflow-hidden">
+        <div className="overflow-hidden">
 
-      {/*  Site header */}
-     
-
-      {/*  Page content */}
-      <main className="flex-grow">
         <Home/>
         {/*  Page sections */}
 
-      </main>
-
-      {/*  Site footer */}
       <Footer />
 
     </div>
